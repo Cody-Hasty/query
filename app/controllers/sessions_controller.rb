ef48a87339819553c1e64543af3170c2222e6ddf
Brittany Hasty <Brittany.R.Hasty@gmail.com>
2020-04-28 11:03:50 -0700

    Almost forgot the sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+    def new
+        # render login page?
+    end
+
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        if @user.nil?
+            # show some errors
+        else
+            login!(@user)
+            # redirect user somewhere...
+        end
+    end
+
+    def destroy
+        logout!
+        # show login page
+    end
+end
