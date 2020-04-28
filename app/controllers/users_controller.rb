87f4f5ad9a2c0ec224eea7c25ca021155a227830
Brittany Hasty <Brittany.R.Hasty@gmail.com>
2020-04-28 11:00:15 -0700

    Users and application controller, will need to be updated after a quick review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,53 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+        # is redirect necessary when combining with frontend?
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+        else
+            # flash.now[:errors] = @user.errors.full_messages
+            # must refresh self on how to display to frontend
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        # once again, does this need redirect?
+        # render json: @user.errors.full_messages, status: 404
+    end
+
+    def index
+        @users = User.all
+    end
+
+    def edit
+        @user = User.find(params[:id])
+        # redirect?
+    end
+
+    def update
+        @user = User.find(params[:id])
+        # render json: @user.errors.full_messages, status: 422
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+         # render json: @user.errors.full_messages, status: 404
+    end
+
+    # def search
+    #     @users = User.where("username LIKE '%#{params[:query]}%'")
+    # end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+
 end
