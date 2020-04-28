36a35dafa158e5c1f351b4254c77b27fb813b3e1
Brittany Hasty <Brittany.R.Hasty@gmail.com>
2020-04-28 11:24:32 -0700

    Going to do some research

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    resources :users
+    # posts to be added later
+    resource :session, only: [:create, :destroy]
+    # post '/search', to 'users#search'
+  end
+  root to: 'root#root'
 end
