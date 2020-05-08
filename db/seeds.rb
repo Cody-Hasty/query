d02fec55d7f0dc3466d0b52bca759bd00471a1c9
Brittany Hasty <Brittany.R.Hasty@gmail.com>
2020-05-08 09:34:37 -0700

    Minor seed data included

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(email: 'demo@demo.com', password: '123456', credentials: "Demo User", fname: "John", lname: "Doe")
-User.create(email: '', password: '', credentials: '', fname: '', lname: '')
+User.create(email: 'cookiemonster@cookie.net', password: 'cookie', credentials: 'Gourmet chef and talk show host', fname: 'Cookie', lname: 'Monster')
+User.create(email: 'tacotuesday@yum.com', password: 'saucey', credentials: 'I eat a LOT of tacos', fname: 'Taco', lname: 'Tuesday')
+User.create(email: 'bobthebuffetman@man.net', password: 'buffet', credentials: 'On a see-food diet', fname: 'Bob', lname: 'BuffetMan')
 
 
 
-Question.create(title: '', body: '', topic: '', author_id: '')
-
-
+Question.create(title: 'COOKIE??!!??', body: 'Does anybody know any good cookie places near Seasame St?', topic: 'food', author_id: '2')
+Question.create(title: 'What day is it?', body: 'I lost my calender, what day of the week is it?', topic: 'time management', author_id: '3')
+Question.create(title: 'Tortilla substitutions?', body: 'I\'m out of tortillas, what could I replace them with in my kitchen?', topic: 'cooking', author_id: '3')
